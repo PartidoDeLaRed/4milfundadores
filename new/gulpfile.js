b263a11ab15f269ed83dd017b399fea02d1420e1
Matias Lescano <matiasj.lescano@gmail.com>
2014-10-08 09:22:25 -0300

    add polyfill for forms

--- new/gulpfile.js
+++ new/gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp')
 var stylus = require('gulp-stylus')
 var nib = require('nib')
 var please = require('gulp-pleeease')
+var browserify = require('gulp-browserify')
+var rename = require('gulp-rename')
+var uglify = require('gulp-uglify')
 
 gulp.task('css', function(){
   return gulp.src('./css/app.styl')
@@ -10,8 +13,24 @@ gulp.task('css', function(){
     .pipe(gulp.dest('./css'))
 })
 
-gulp.task('w', function(){
-  gulp.watch('./**/*.styl', ['css'])
+gulp.task('js', function() {
+  gulp.src('./js/app.js')
+    .pipe(browserify({
+      shim: {
+        H5F: {
+          path: './js/lib/H5F.js',
+          exports: 'H5F'
+        }
+      }
+    }))
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('./js'))
 })
 
-gulp.task('default', ['css']);
+gulp.task('w', function(){
+  gulp.watch('./css/**/*.styl', ['css'])
+  gulp.watch('./js/**/*.js', ['js'])
+})
+
+gulp.task('default', ['css', 'js'])
---
+++ new/js/app.js
@@ -0,0 +1,3 @@
+var H5F = require('H5F')
+
+H5F.setup(document.querySelector('form'))
